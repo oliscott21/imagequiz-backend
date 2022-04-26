450eddeb27d1e6dcd9f23bffb8c51842ea6f747a
oliscott21 <oliscott21@gmail.com>
2022-04-26 15:46:22 -0700

    fix

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ app.use(cors({
 app.use((request, response, next) => {
     console.log(`request url: ${request.url}`);
     console.log(`request method: ${request.method}`);
-    //only for development. Remove this line when you deploy your final version.
-    console.log(`request body: ${request.body}`);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    request.header("Access-Control-Allow-Origin", "*");
+    request.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
 
